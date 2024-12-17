a8f7ffbdf081c469a4230b9c33ce3bc6fbfe757b
Louis Garoche <louis.garoche@ubisoft.com>
2024-12-17 18:10:02 +0100

    fix: gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,7 +9,7 @@ import { IDCardContent } from "@/model";
 export default function Gallery() {
 
     const [cards, setCards] = useState([...Array(10_000)])
-    const [messages, setMessages] = useState<PostResponse<IDCardContent>[]>([...Array(10_000)])
+    const [messages, setMessages] = useState<PostResponse<IDCardContent>[]>([])
 
     useEffect(() => {
         const client = new AlephHttpClient()
@@ -28,19 +28,19 @@ export default function Gallery() {
     useEffect(() => {
         const cards = [...Array(10_000)]
         for (const message of messages) {
-            //console.log(message)
             const id = parseInt((message as unknown as { ref: string }).ref.split(":")[1])
             cards[id] = message.content.image.replace("ipfs://", "https://ipfs.bytes32.net/ipfs/")
         }
+        console.log(cards)
         setCards(cards)
     }, [messages])
 
 
     return (
         <div className="m-auto flex flex-wrap justify-center items-center min-h-screen">
-            {Array(10_000).map((_, index) => (
+            {cards.map((c, index) => (
                 <Link href={`/details/?tokenId=${index}`} key={index}>
-                    <Image alt={`card${index}`} width={16} height={20} src={cards[index] ?? `/placeholder_single.png`} key={index} />
+                    <Image alt={`card${index}`} width={16} height={20} src={c ?? `/placeholder_single.png`} key={index} />
                 </Link>
             ))}
         </div>
